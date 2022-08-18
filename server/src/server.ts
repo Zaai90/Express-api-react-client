0887301c4bbefd506ed1a0181a5ed31f1f1e7560
Zaai90 <erik.grune@gmail.com>
2022-08-18 21:27:25 +0200

    add userRoutes and error and notfound middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express';
-import productRouter from './product/product.router';
+require('express-async-errors');
+
+import { errorHandler, notFoundHandler } from './resources/middlewares';
+import productRouter from './resources/products/product.router';
+import userRouter from './resources/users/users.router';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-//Middlewares
 app.use(express.json(), express.urlencoded({ extended: true }));
 app.use('/api/products', productRouter);
-
-
+app.use('/api/users', userRouter);
+app.use(notFoundHandler, errorHandler);
 app.listen(port, () => {
   console.log(`listening on: http://localhost:${port}`);
 });
