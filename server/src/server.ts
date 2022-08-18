64678eda60d602d655a111353ede6ee9c34a06ef
Erik <erik.grune@gmail.com>
2022-08-18 09:57:56 +0200

    remove static

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //Middlewares
-app.use(express.static("public"));
 app.use(express.json(), express.urlencoded({ extended: true }));
 app.use('/api/products', productRouter);
 
