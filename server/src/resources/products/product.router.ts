86d90935a62eeb61e13b06b924ad05e60a77c049
Zaai90 <erik.grune@gmail.com>
2022-08-18 21:36:18 +0200

    add vaildation of Id

--- server/src/resources/products/product.router.ts
+++ server/src/resources/products/product.router.ts
@@ -7,12 +7,13 @@ import {
   updateProduct,
   validate,
 } from "./product.controller";
+import { validateProductId } from "./product.validators";
 
 const router = express.Router();
 router.post("/", validate, createProduct);
 router.get("/get", getProducts);
-router.put("/:id", validate, updateProduct);
-router.delete("/delete/:id", validate, deleteProduct);
-router.get("/:id", validate, getProduct);
+router.put("/:id", validate, validateProductId, updateProduct);
+router.delete("/delete/:id", validate, validateProductId, deleteProduct);
+router.get("/:id", validate, validateProductId, getProduct);
 
 export default router;
