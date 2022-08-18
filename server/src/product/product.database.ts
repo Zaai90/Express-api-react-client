153c9e69e0292174f91b9dff79afc4c1195f603d
Erik <erik.grune@gmail.com>
2022-08-18 09:58:26 +0200

    add load and save to db file

--- server/src/product/product.database.ts
+++ server/src/product/product.database.ts
@@ -1,6 +1,9 @@
 import Product from './product.model';
+import * as fs from 'fs';
 
-export let products : Product[] = [];
+export let products: Product[] = [];
+load();
+
 
 export function getItems() {
     return products;
@@ -12,6 +15,7 @@ export function getItem(id: string) {
 
 export function addItem(product: Product) {
     products.push(product);
+    save()
 }
 
 export function updateItem(id: string, product: Product) {
@@ -19,9 +23,31 @@ export function updateItem(id: string, product: Product) {
     let updatedProduct: Product = product;
     updatedProduct.id = id;
     products[index] = updatedProduct;
+    save();
 }
 
 export function deleteItem(id: string) {
     const index = products.findIndex(p => p.id === id);
     products.splice(index, 1);
+    save();
+}
+
+function save() {
+    fs.writeFile('./db.json', JSON.stringify(products, null, '\t'), (err: any) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Saved successfully');
+        }
+    });
+}
+
+function load() {
+    fs.readFile('./db.json', 'utf-8', (err: NodeJS.ErrnoException | null, data: string) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            products = JSON.parse(data);
+        }
+    });
 }
