a5bde12c4e29c6ab77b30640c96296f2f4bd4117
Zaai90 <erik.grune@gmail.com>
2022-08-22 21:56:42 +0200

    add validate productnotexist

--- server/src/resources/products/product.router.ts
+++ server/src/resources/products/product.router.ts
@@ -8,10 +8,10 @@ import {
   updateProduct,
   validate
 } from "./product.controller";
-import { validateProductId } from "./product.validators";
+import { validateProductId, validateProductNotExist } from "./product.validators";
 
 const router = express.Router();
-router.post("/", isLoggedIn, isAdmin, validate, createProduct);
+router.post("/", isLoggedIn, isAdmin, validate, validateProductNotExist, createProduct);
 router.get("/", getProducts);
 router.put("/:id", isLoggedIn, isAdmin, validateProductId, validate, updateProduct);
 router.delete("/:id", isLoggedIn, isAdmin, validateProductId, deleteProduct);
