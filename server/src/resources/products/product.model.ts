93e4e2c2832fd759ac3b1a3fd876e6bd431152a8
Zaai90 <erik.grune@gmail.com>
2022-08-22 21:57:10 +0200

    add extra props for product

--- server/src/resources/products/product.model.ts
+++ server/src/resources/products/product.model.ts
@@ -1,14 +1,25 @@
 import Joi from "joi";
 
 export interface Product {
-    id?: string;
-    name: string;
-    price: number;
-  };
-
+  id?: string;
+  name: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  version?: number;
+};
 
 export const productSchema = Joi.object<Product, true>({
-    id: Joi.string().optional(),
-    name: Joi.string().required().min(3).max(50),
-    price: Joi.number().required().min(1).max(1000)
+  id: Joi.string(),
+  name: Joi.string().required().min(3).max(50),
+  price: Joi.number().required().min(1).max(1000),
+  category: Joi.string().required().min(3).max(50),
+  description: Joi.string().required().min(3).max(1000),
+  image: Joi.string().min(3).max(1000),
+  createdAt: Joi.date(),
+  updatedAt: Joi.date(),
+  version: Joi.number()
 });
