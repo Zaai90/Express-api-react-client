df81c674b1b248efac15aec06f25aef879a1a90d
Zaai90 <erik.grune@gmail.com>
2022-08-26 00:02:44 +0200

    remove user endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,14 +3,12 @@ require('express-async-errors');
 
 import { errorHandler, notFoundHandler } from './resources/middlewares';
 import productRouter from './resources/products/product.router';
-import userRouter from './resources/users/users.router';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json(), express.urlencoded({ extended: true }));
 app.use('/api/products', productRouter);
-app.use('/api/users', userRouter);
 app.use(notFoundHandler, errorHandler);
 app.listen(port, () => {
   console.log(`listening on: http://localhost:${port}`);
